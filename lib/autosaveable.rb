69d2e3ab93e065f3fa9df2fed732fc69c03f2959
dan-hoerr <dhoerr@gmail.com>
2013-03-30 15:13:32 -0400

    added rollback capabilities to autosave sidebar
    Signed-off-by: dan-hoerr <dhoerr@gmail.com>

--- lib/autosaveable.rb
+++ lib/autosaveable.rb
@@ -136,11 +136,18 @@ module AutoSaveable
       base.send :member_action, :versions
 
       base.sidebar :autosaves, :only => :edit do
+        resource_string = resource_class.to_s.downcase
+        current_resource = eval(resource_string)
+
         ul do
-          li "Second List First Item"
-          li "Second List Second Item"
+          if !current_resource.autosaves.empty?
+            current_resource.autosaves.each do |autosave|
+              li link_to "Autosave #{autosave.created_at.to_s :long_ordinal}", send("edit_admin_#{resource_string.downcase}_path", :autosave_id => autosave.id) 
+            end
+          else
+            li "No autosaves yet." 
+          end
         end
-        p "we could put autosaves here"
       end
 
       base.controller do
@@ -157,10 +164,16 @@ module AutoSaveable
         def edit
           resource_string = resource_class.to_s.downcase
           current_resource = self.instance_variable_get "@#{resource_string}"
-          last_version = current_resource.autosaves.last
-          if last_version && (last_version.created_at > current_resource.updated_at)
-            instance_variable_set "@#{resource_string}", last_version.reify
-            flash.now[:notice] = "Auto-restored a more recent autosave. Live version of this #{resource_string} is unaffected."
+          if params[:autosave_id]
+            version_to_replace_with = current_resource.autosaves.find(params[:autosave_id])
+            instance_variable_set "@#{resource_string}", version_to_replace_with.reify
+            flash.now[:notice] = "Restored Autosave ##{params[:autosave_id]}"
+          else
+            last_version = current_resource.autosaves.last
+            if last_version && (last_version.created_at > current_resource.updated_at)
+              instance_variable_set "@#{resource_string}", last_version.reify
+              flash.now[:notice] = "Auto-restored a more recent autosave. Live version of this #{resource_string} is unaffected."
+            end
           end
         end
       end
